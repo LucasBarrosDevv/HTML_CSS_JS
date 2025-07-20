f78eda56d705f0210dd8916d872e3a104cfbc286
LucasBarrosDevv <lucasbarrosbm.dev@gmail.com>
2025-07-19 21:39:51 -0300

    >

--- Projects/Python/meus_backends/app.py
+++ Projects/Python/meus_backends/app.py
@@ -6,6 +6,8 @@ import yt_dlp
 app = Flask(__name__)
 CORS(app)  # Permite CORS para todas as rotas
 
+COOKIE_FILE_PATH = 'cookies.txt'  # Arquivo de cookies exportado do navegador
+
 @app.route('/get_video', methods=['POST'])
 def get_video():
     data = request.get_json()
@@ -14,19 +16,37 @@ def get_video():
 
     url = data['url']
 
+    # Configurações do yt_dlp
     ydl_opts = {
         'format': 'best',
         'quiet': True,
     }
 
+    # Se cookies.txt existir, adiciona para autenticação
+    if os.path.isfile(COOKIE_FILE_PATH):
+        ydl_opts['cookiefile'] = COOKIE_FILE_PATH
+
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            # Extrai as informações do vídeo sem baixar
             info = ydl.extract_info(url, download=False)
-            video_url = info['url']
+
+            # Pega a URL direta do stream, título, thumbnail e duração
+            video_url = info.get('url')
+            title = info.get('title')
+            thumbnail = info.get('thumbnail')
+            duration = info.get('duration')
+
     except Exception as e:
         return jsonify({'error': 'Erro ao extrair vídeo', 'details': str(e)}), 500
 
-    return jsonify({'stream_url': video_url})
+    # Retorna dados em JSON para o frontend
+    return jsonify({
+        'stream_url': video_url,
+        'title': title,
+        'thumbnail': thumbnail,
+        'duration': duration
+    })
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
